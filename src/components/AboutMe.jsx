b4034538358ac57266389e0d07253de125d0df8e
MariiaKpln <kpln.mary@gmail.com>
2025-07-08 22:32:52 +0300

    homework

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import {base_url, details} from "../utils/constants.js";
+
 const AboutMe = () => {
+    const [aboutMe, setAboutMe] = useState();
+
+    useEffect(() => {
+        fetch(`${base_url}/v1/peoples/1`)
+            .then(res => res.json())
+            .then(setAboutMe);
+    }, []);
+
+    if (!aboutMe) {
+        return (
+            <div className="about-me">
+                <span className="spinner-border spinner-border-sm"></span> Loading...
+            </div>
+        );
+    }
+
+
+
     return (
-        <div>
-            About me
+        <div className="about-me">
+            <h1>About me</h1>
+            <ul>
+                {details.map(({ label, value }) => (
+                    <li key={label}>
+                        <strong>{label}:</strong> {value}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
 
 export default AboutMe;
+
+
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from "react";
+import {base_url} from "../utils/constants.js";
+
 const Contact = () => {
+    const [planets, setPlanets] = useState([]);
+
+    useEffect(() => {
+        fetch(`${base_url}/v1/planets`)
+            .then(res => res.json())
+            .then(data => setPlanets(data));
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            Contact
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..." />
+
+                <label htmlFor="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..." />
+
+                <label htmlFor="planet">Planet</label>
+                <select id="planet" name="planet">
+                    {planets.map((planet, index) => (
+                        <option key={index} value={planet.name}>
+                            {planet.name}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="subject">Subject</label>
+                <textarea
+                    id="subject"
+                    name="subject"
+                    placeholder="Write something..."
+                    style={{ height: '200px' }}
+                />
+
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
 
 export default Contact;
+
+
+
+
+
